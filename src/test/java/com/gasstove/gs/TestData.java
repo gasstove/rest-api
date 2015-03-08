657d82c36bddd6ac79f2718abb334805580e3e66
Sean Morris <sean.smorris@gmail.com>
2015-03-08 14:33:40 -0700

    All test data in db

--- src/test/java/com/gasstove/gs/TestData.java
+++ src/test/java/com/gasstove/gs/TestData.java
@@ -7,26 +7,41 @@ import java.util.GregorianCalendar;
  * This is the script to populate the test database
  */
 public class TestData {
+
+    /**
+     * Main method to load test data
+     *
+     * @param args
+     */
+    public static void main(String[] args){
+        TestData t = new TestData();
+        t.getConnection();
+        t.removeAllRecords();
+        t.addData();
+    }
+
+    //set these values to manipulate test records
     final int NUM_EVENTS_USER = 5;
     final int NUM_MEDIA_PER_EVENT_PER_USER = 5;
     final int RANDOM_YEAR_START = 2014;
     final int RANDOM_YEAR_END = 2016;
     final int NUM_ACTORS = 10;
 
+
     String dbConnect = "jdbc:sqlite:/Users/seanmorris/Development/db/gasstove.db";
     //jdbc:sqlite:/Users/smorris/Development/db/gasstove.db
+
     Connection connection = null;
     ResultSet resultSet = null;
     PreparedStatement  statement = null;
     Statement stmt = null;
 
-    public static void main(String[] args){
-        TestData t = new TestData();
-        t.getConnection();
-        t.removeAllRecords();
-        t.addActors();
 
-    }
+    /**
+     * Establish connection to db
+     *
+     * @return Connection database connection
+     */
     public Connection getConnection(){
         try
         {
@@ -41,18 +56,9 @@ public class TestData {
 
     }
 
-    public java.util.Date randomDate(){
-        GregorianCalendar gc = new GregorianCalendar();
-        int year = randBetween(RANDOM_YEAR_START,RANDOM_YEAR_END);
-        gc.set(gc.YEAR, year);
-        int dayOfYear = randBetween(1, gc.getActualMaximum(gc.DAY_OF_YEAR));
-        gc.set(gc.DAY_OF_YEAR, dayOfYear);
-        return gc.getTime();
-    }
-
-    public static int randBetween(int start, int end) {
-        return start + (int)Math.round(Math.random() * (end - start));
-    }
+    /**
+     * Removes all the records from each table so they can be populated
+     */
     public void removeAllRecords(){
         try {
             String sql = "DELETE FROM roles";
@@ -83,7 +89,20 @@ public class TestData {
         }
 
     }
-    public void addActors(){
+
+    /**
+     * Add all test data to tables. See final attributes defined to
+     * manipulate number of records and such.
+     *
+     * The process for adding a records:
+     *      - add an actor
+     *      - add a bunch of events for the actor
+     *      - for every event add record to actor_event_mapping
+     *      - for every event add media
+     *      - for every media add record to media_mapping using media id and actor_event_mapping id
+     *      - repeat steps above for number of actors
+     */
+    public void addData(){
         try {
             int eventIndex = 0;
             int actorIndex = 0;
@@ -92,11 +111,7 @@ public class TestData {
             int currentActorEventMappingIndex = -1;
             int currentMediaIndex = -1;
 
-            //add actor
-            //add event
-            //add actor event mapping
-            //add media
-            //add media mapping
+            //add the roles you want defined
             String sql = "INSERT into roles(id,type) VALUES(?,?)";
             statement = connection.prepareStatement(sql);
             statement.setInt(1, 1);
@@ -109,7 +124,10 @@ public class TestData {
             statement.setString(2, "member");
             statement.execute();
 
+            //this will loop for the number of actors defined
             for(int f = 0; f < NUM_ACTORS; f++, actorIndex++) {
+
+                //insert an actor
                 sql = "INSERT into actor(first,last,is_subscriber,contact_method) VALUES(?,?,1,?)";
                 statement = connection.prepareStatement(sql);
                 String[] name = rNames[actorIndex].split(" ");
@@ -118,6 +136,7 @@ public class TestData {
                 statement.setInt(3, (int) (Math.random() * 2));
                 statement.execute();
 
+                //get actor id
                 sql = "SELECT Max(id) from actor";
                 stmt = connection.createStatement();
                 ResultSet r = stmt.executeQuery(sql);
@@ -125,9 +144,8 @@ public class TestData {
                     currentActorIndex = r.getInt(1);
                 }
 
+                //insert events for actor
                 for (int x = 0; x < NUM_EVENTS_USER; x++, eventIndex++) {
-
-
                     sql = "INSERT into event(name,open_date,close_date,join_invitation,join_allow_by_accept,join_allow_auto) VALUES(?,?,?,1,1,1)";
                     statement = connection.prepareStatement(sql);
                     statement.setString(1, events[eventIndex]);
@@ -135,6 +153,7 @@ public class TestData {
                     statement.setDate(3, new java.sql.Date(randomDate().getTime() + randBetween(5, 10)));
                     statement.execute();
 
+                    //get event id
                     sql = "SELECT Max(id) from event";
                     stmt = connection.createStatement();
                     r = stmt.executeQuery(sql);
@@ -142,6 +161,7 @@ public class TestData {
                         currentEventIndex = r.getInt(1);
                     }
 
+                    //add mapping for actor and event
                     sql = "INSERT into actor_event_mapping(event_id, actor_id,role_id,join_date) VALUES(?,?,?,?)";
                     statement = connection.prepareStatement(sql);
                     statement.setInt(1, currentEventIndex);
@@ -150,6 +170,7 @@ public class TestData {
                     statement.setDate(4, new java.sql.Date(randomDate().getTime()));
                     statement.execute();
 
+                    //get actor_event mapping id for media_mapping table
                     sql = "SELECT Max(id) from actor_event_mapping";
                     stmt = connection.createStatement();
                     r = stmt.executeQuery(sql);
@@ -157,14 +178,16 @@ public class TestData {
                         currentActorEventMappingIndex = r.getInt(1);
                     }
 
+                    //add media for this event and actor
                     for (int k = 0; k < NUM_MEDIA_PER_EVENT_PER_USER; k++) {
+                        //insert the media
                         sql = "INSERT into media(type, file_name) VALUES(?,?)";
                         statement = connection.prepareStatement(sql);
                         statement.setString(1, mediaTypes[(int) (Math.random() * 3)]);
                         statement.setString(2, "media_" + (int) (Math.random() * 10000));
-
                         statement.execute();
 
+                        //get media id
                         sql = "SELECT Max(id) from media";
                         stmt = connection.createStatement();
                         r = stmt.executeQuery(sql);
@@ -172,13 +195,13 @@ public class TestData {
                             currentMediaIndex = r.getInt(1);
                         }
 
+                        //add the media mapping using media id and actor_event_mapping id
                         sql = "INSERT into media_mapping(media_id, actor_event_mapping_id,num_downloads,shared) VALUES(?,?,?,?)";
                         statement = connection.prepareStatement(sql);
                         statement.setInt(1, currentMediaIndex);
                         statement.setInt(2, currentActorEventMappingIndex);
                         statement.setInt(3, randBetween(10, 10000));
                         statement.setInt(4, randBetween(0, 2));
-
                         statement.execute();
                     }
                 }
@@ -189,6 +212,33 @@ public class TestData {
             s.printStackTrace();
         }
 
+    }
+
+    /**
+     * Generate a random date base on RANDOM_YEAR_START and RANDOM_YEAR_END
+     *
+     * @return java.util.Date
+     */
+    private java.util.Date randomDate(){
+        GregorianCalendar gc = new GregorianCalendar();
+        int year = randBetween(RANDOM_YEAR_START,RANDOM_YEAR_END);
+        gc.set(gc.YEAR, year);
+        int dayOfYear = randBetween(1, gc.getActualMaximum(gc.DAY_OF_YEAR));
+        gc.set(gc.DAY_OF_YEAR, dayOfYear);
+        return gc.getTime();
+    }
+
+    /**
+     *
+     * Generate random number in range.
+     *
+     * @param start beginning of range, inclusive
+     * @param end end of range, exclusive
+     *
+     * @return random number between start(inclusive) and end(exclusive)
+     */
+    private static int randBetween(int start, int end) {
+        return start + (int)Math.round(Math.random() * (end - start));
     }
 
     static String[] rNames= {
