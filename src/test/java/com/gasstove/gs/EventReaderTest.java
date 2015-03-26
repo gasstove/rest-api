8fdf2722c93244f49ae9f8b3ada4ea6ee379dee2
Sean Morris <sean.smorris@gmail.com>
2015-03-25 17:33:54 -0700

    added tests for EventReader

--- src/test/java/com/gasstove/gs/EventReaderTest.java
+++
@@ -1,9 +0,0 @@
-package com.gasstove.gs;
-
-/**
- * Created by smorris on 3/24/15.
- */
-public class EventReaderTest {
-
-
-}
---
+++ src/test/java/com/gasstove/gs/test/dbaccess/EventReaderTest.java
@@ -0,0 +1,72 @@
+package com.gasstove.gs.test.dbaccess;
+
+import com.gasstove.gs.dbaccess.EventReader;
+import com.gasstove.gs.models.Event;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+/**
+ * Tests methods for EventReader
+ *
+ */
+public class EventReaderTest {
+    private EventReader er = new EventReader();
+    /**
+     * The constructor sets up database for so we can set up a Model DataBase Access
+     * reader and writer;
+     *
+     * @throws Exception This happens if the database set up has trouble
+     */
+    public EventReaderTest() throws Exception {
+
+    }
+
+    /**
+     * This tests the get networks method. The method should return a list of networks.
+     * We don't bother parsing back the response from JSON in this case.
+     */
+    @Test
+    public void testGetEvents() {
+        try {
+            EventReader er = new EventReader();
+            ArrayList<Event> list = er.getEvents();
+
+            assertTrue(list.size() > 0);
+            assertTrue(list.get(0).getName() != null);
+
+        } catch (Exception exp) {
+            exp.printStackTrace();
+            fail();
+        }
+
+    }
+
+    /**
+     * This tests the get event method. The method should return am event
+     */
+    @Test
+    public void testGetEvent() {
+
+        try {
+
+            Event e = er.getEvent(537);
+            assertTrue(e.getName().length() > 0);
+            assertTrue(e.getId() == 537);
+            assertNotNull(e.getUsers());
+            assertNotNull(e.getCloseDate());
+            assertNotNull(e.getOpenDate());
+
+
+        } catch (Exception exp) {
+            exp.printStackTrace();
+            fail();
+        }
+
+    }
+
+}
