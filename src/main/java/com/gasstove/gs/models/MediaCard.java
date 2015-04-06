d40099825a4702aeb39b0a1bb94b3e229fd45cd2
gasstove <grabielgomes@gmail.com>
2015-04-06 15:46:00 -0700

    media card

--- src/main/java/com/gasstove/gs/models/MediaCard.java
+++ src/main/java/com/gasstove/gs/models/MediaCard.java
@@ -4,59 +4,11 @@ package com.gasstove.gs.models;
  * Created by smorris on 3/24/15.
  */
 public class MediaCard {
-    private Media media;
-    private int numDownloads;
-    private boolean shared;
-    private String comment;
-    private int numLikes;
-    private int numDislikes;
-
-
-    public Media getMedia() {
-        return media;
-    }
-
-    public void setMedia(Media media) {
-        this.media = media;
-    }
-
-    public int getNumDownloads() {
-        return numDownloads;
-    }
-
-    public void setNumDownloads(int numDownloads) {
-        this.numDownloads = numDownloads;
-    }
-
-    public boolean isShared() {
-        return shared;
-    }
-
-    public void setShared(boolean shared) {
-        this.shared = shared;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    public int getNumLikes() {
-        return numLikes;
-    }
-
-    public void setNumLikes(int numLikes) {
-        this.numLikes = numLikes;
-    }
-
-    public int getNumDislikes() {
-        return numDislikes;
-    }
-
-    public void setNumDislikes(int numDislikes) {
-        this.numDislikes = numDislikes;
-    }
+    public Media media;
+    public Event event;
+    public int numDownloads;
+    public boolean shared;
+    public String comment;
+    public int numLikes;
+    public int numDislikes;
 }
